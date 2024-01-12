509708d498478cd91b7630b1d8ac401830ee72f8
sikwan <yysskk99999@naver.com>
2024-01-12 14:44:14 +0900

    LoginView with UIKIt

--- KTV/ViewController.swift
+++ KTV/ViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  LoginViewController.swift
 //  KTV
 //
 //  Created by 양시관 on 1/12/24.
@@ -7,13 +7,22 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class LoginViewController: UIViewController {
 
+    @IBOutlet var imageView: UIImageView!
+    @IBOutlet var loginButton: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        self.loginButton.layer.cornerRadius = 19
+        self.loginButton.layer.borderColor = UIColor(named: "main-brown")?.cgColor
+        self.loginButton.layer.borderWidth = 1
     }
 
-
+     
+    
+    @IBAction func buttonDidTap(_ sender: Any) {
+    }
 }
 
